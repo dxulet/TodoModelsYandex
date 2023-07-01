95233b155a1dd7dae37f0f92ba03a654bf84df80
Daulet Ashikbayev <dxulet@gmail.com>
2023-07-01 20:01:28 +0600

    init commit

--- Package.swift
+++ Package.swift
@@ -21,8 +21,5 @@ let package = Package(
         .target(
             name: "TodoModelsYandex",
             dependencies: []),
-        .testTarget(
-            name: "TodoModelsYandexTests",
-            dependencies: ["TodoModelsYandex"]),
     ]
 )
